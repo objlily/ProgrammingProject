d3d10d2b8db02e87adc34f0f47620f54c4d42635
lucasfng <67241708+lucasfng@users.noreply.github.com>
2025-06-02 12:56:17 -0700

    Update account.py

--- account.py
+++ account.py
@@ -1,14 +1,31 @@
 allUsers = {}
 currentUser = None
+
 def signUp(userName, password):
   if userName in allUsers:
     return False
-  allUsers[userName] = password
+  allUsers[userName] = User(password)
   return True
 def signIn(userName, password):
-  if allUsers[userName] == password:
+  if allUsers[userName].password == password:
     currentUser = userName
-    return True
   return False
 def signOut():
   currentUser = None
+
+def createNewAccount(accountName, sum):
+  currentUser.accounts[accountName] = sum
+def checkAccount(accountName):
+  return currentUser.accounts[accountName]
+
+
+class User:
+  def __init__(self, password):
+    self.password = password
+    accounts = {}
+
+
+class Account:
+  def __init__(self, accountName, sum):
+    self.accountName = accountName
+    self.sum = sum
